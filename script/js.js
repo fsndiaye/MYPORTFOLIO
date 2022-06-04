c2cb726989a9275c4df176710afa5168d0bf8bfe
fsndiaye <fsndiaye@student.rtc.edu>
2022-06-03 21:41:16 -0700

    TextTypingAnimation

--- script/js.js
+++ script/js.js
@@ -1,1 +1,7 @@
-                                          
+ // ============================  Typing Animation   ==========================
+ var typed = new Typed(".typing",{
+     strings:["","Web Designer","Web Developer","Programer","Computer Science Student"],
+     typeSpeed:100,
+     BackSpeed:60,
+     loop:true
+ })                                  
